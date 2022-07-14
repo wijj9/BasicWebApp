ca2029afaeb74d523f9ad7bc913e70a516fd9d69
Wajih Tarkhani <71191955+wijj9@users.noreply.github.com>
2022-07-14 12:45:15 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -8,7 +8,7 @@ public class QueryProcessor {
     public String process(String query) {
 		query = query.toLowerCase();
         if (query.contains("shakespeare")) {
-            return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
+            return "William Shakespeare (26 April 1564 - 28 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
